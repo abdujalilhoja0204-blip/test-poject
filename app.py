a8dabc579aa9f221bc27fd54f0e2e060d5049f7c
abdujalil <abdujalilhoja0204@gmail.com>
2025-12-11 20:19:15 +0500

    Update home route response message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "CI/CD with Docker works!"
+    return "CI/CD with Docker works! CI/CD Ishladi"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
